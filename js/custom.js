fc1c04694822783bbeb583b873f61e95f1cc9a25
Eduard <o.eduardweb@gmail.com>
2018-02-14 15:32:01 +0200

    accordion

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+function ready() {
+  
+//https://www.npmjs.com/package/jq-accordion
+  $('.accordion').accordion({
+   "transitionSpeed": 400
+   
+ });
+
+
+}
+
+document.addEventListener("DOMContentLoaded", ready);
