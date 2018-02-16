9ba0e940ad795dc38861f31d6475eff22555dd6e
Eduard <o.eduardweb@gmail.com>
2018-02-17 01:35:27 +0200

    add settings

--- js/custom.js
+++ js/custom.js
@@ -3,10 +3,7 @@ function ready() {
 //https://www.npmjs.com/package/jq-accordion
   $('.accordion').accordion({
    "transitionSpeed": 400
-   
- });
-
-
+  });
 }
 
 document.addEventListener("DOMContentLoaded", ready);
